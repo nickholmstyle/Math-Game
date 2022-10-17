0610e7fc684acffb4c4798f048595cb5d04f93ac
nickholmstyle <nick416holm@protonmail.com>
2022-10-17 18:06:33 +0000

    got the numbers adding for a turn

--- main.rb
+++ main.rb
@@ -1,3 +1,16 @@
 require './player.rb'
 require './questions.rb'
 
+player1 = Player.new("Player 1")
+player2 = Player.new("Player 2")
+
+# puts player1.name
+# puts player2.name
+questions = Questions.new()
+
+while player1.lives != 0 and player2.lives != 0
+   puts "---- NEW TURN ----"
+   questions.newQuestion()
+   puts questions.question
+   player1_answer = gets.chomp.to_i
+end
